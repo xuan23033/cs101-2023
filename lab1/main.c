fde857392c3759f99cf3b902fbdceed2e0b381fb
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2023-03-09 14:05:21 +0800

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    
+    File* fp;
+    int a[3] = {1, 2, 3};
+    int arr_read[5];
+    
+    if ((fp = fopen("a.bin", "wb+")) == NULL) {
+       
+        exit(1);
+    }
+    
+    fwrite(a, sizeof (a), 1, fp);
+    
+    fseek(fp, 0, SEEK_SET);
+    fread(arr_read, sizeof(arr_read), 1, fp);
+    
+    return 0;
+}
 

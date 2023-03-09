e4896977d8cbee0372c62f1fe0cdd0e3ad92fa59
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2023-03-09 14:06:47 +0800

    Update main.c

--- lab1/main.c
+++ lab1/main.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    
-    
-    File* fp;
-    int a[3] = {1, 2, 3};
-    int arr_read[5];
-    
+    FILE* fp;
+    int a[] = {1,2,3} ;
+    int a_read[sizeof(a)/sizeof(a[0])];
+    char b[] = "ABC" ;
+    float c[] = {1.1, 1.2, 1.3} ;
     if ((fp = fopen("a.bin", "wb+")) == NULL) {
-       
+        printf("Cannot open the file...") ;
         exit(1);
     }
-    
-    fwrite(a, sizeof (a), 1, fp);
-    
+    if (fwrite(a, sizeof(a), 1,fp) != 1) {
+        printf("File write error....\n");
+    }
     fseek(fp, 0, SEEK_SET);
-    fread(arr_read, sizeof(arr_read), 1, fp);
-    
-    return 0;
-}
 
+    if (fread(a_read,sizeof(a_read), 1, fp) != 1) {
+        printf("File read error....\n") ;
+    }
+    for (int i = 0 ;i < (sizeof(a)/sizeof(a[0])) ;i++) {
+        printf("%d",a_read[i]);
+    }
+    return 0 ;
+}

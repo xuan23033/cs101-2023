253f6a33aff601d2c82b3530e7d61b5854996725
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2023-03-16 14:51:38 +0800

    Add files via upload

--- lab3/main.c
+++ lab3/main.c
@@ -19,5 +19,17 @@ int main() {
         fwrite(p+i, sizeof(person_t), 1, wfp);
         
     }
-    fclose(wfp);                  
+    fclose(wfp);
+    
+    FILE* rfp = fopen("person.bin", "r");
+    person_t tmp[1];
+    int i = 0;
+    while (fread(tmp, sizeof(person_t), 1, rfp)) {
+        printf("[%d] %d: %s %s\n", i,
+                                    tmp[0].id,
+                                    tmp[0].first_name,
+                                    tmp[0].last_name);
+        i++;
+    }
+    fclose(rfp);
 }

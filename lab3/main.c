04752b47f660632d81521b17eae23e18a5610945
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2023-03-16 14:45:58 +0800

    Update main.c

--- lab3/main.c
+++ lab3/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+typedef struct person {
+    int id;
+    char first_name[32];
+    char last_name[32];
+    
+} person_t;
 
+int main() {
+    FILE* wfp;
+    wfp = fopen("person.bin", "w+");
+    person_t p[5] = { {1, "IU", "Lee"},
+                      {2, "IUU", "Lee"},
+                      {3, "IUUU", "Lee"},
+                      {4, "IUUUU", "Lee"},
+                      {5, "IUUUUU", "Lee"},
+    };
+    for (int i=0;i<5;i++) {
+        fwrite(p+i, sizeof(person_t), 1, wfp);
+        
+    }
+    fclose(wfp);                  
+}

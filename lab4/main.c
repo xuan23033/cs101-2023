9365898fc4269af1db732b5e5466e494eed1012b
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2023-03-23 14:21:34 +0800

    Add files via upload

--- lab4/main.c
+++ lab4/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+void replace_all(char* str, char oldChar, char newChar) {
+    
+    while(*str != '\0') {
+        if(*str == oldChar) {
+            *str = newChar;
+        }
+        str++;
+    }
+    
+}
 
+int main() {
+    char str[100], oldChar = 'a', newChar = 'A';
+    printf("Enter any string: ");
+    scanf("%s", str);
+    
+    printf("\nString before replacing: \n%s", str);
+    replace_all(str, oldChar, newChar);
+    printf("\n\nString after replacing %c with %c : \n%s", oldChar, newChar, str);
+    
+    return 0;
+}

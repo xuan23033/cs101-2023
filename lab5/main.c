be4fc1079e505e23a37156b24154ccd623a1f76f
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2023-03-30 15:38:30 +0800

    Add files via upload

--- lab5/main.c
+++ lab5/main.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+typedef int (*calc_func)(int, int);
+
+int add(int x, int y) {
+    return x+y;
+    
+}
+int sub(int x, int y) {
+    return x-y;
+    
+}
+int mul(int x, int y) {
+    return x*y;
+}
+int div(int x, int y) {
+    return x/y;
+    
+}
+int calc(calc_func cacl_ptr, int a, int b) {
+    return cacl_ptr(a,b);
+}
+
+
+
+int main() {
+    
+    printf("sum = %d\n", calc(add, 4, 2));
+    printf("dif = %d\n", calc(sub, 4,2));
+    printf("product = %d\n", calc(mul, 4, 2));
+    printf("quotient = %d\n", calc(div, 4, 2));
+    return 0;
+}

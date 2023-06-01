27b65b8e8e4105729c1f871cfed619f3559aa7dd
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2023-06-01 14:37:15 +0800

    Add files via upload

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
+#include <fstream>
 
+using namespace std;
+
+class myString {
+    private:
+        string m_str;
+    public:
+        myString(string s) {
+            m_str = s;
+        }
+};
+
+class ReadClass {
+    
+    
+    void showClass() {
+        ifstream in;
+        string line;
+        in.open("main.cpp");
+        if (in.fail()) {
+            cout << "Error opeing a file" << endl;
+            in.close();
+        }
+    
+        while (getline(in, line)) {
+            cout << line << endl;
+        }
+        in.close();
+    }
+    
+};
+int main() {
+    
+    ReadClass rfile;
+    rfile.showClass();
+    return 0;
+}
